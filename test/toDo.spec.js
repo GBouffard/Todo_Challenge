0b9bd7846c73967a2190db58c17aaf1b7cc19ecb
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-25 23:15:14 +0100

    Written Karma tests (by-passed for now)

---
+++ test/toDo.spec.js
@@ -0,0 +1,35 @@
+describe('guillaumeToDo', function() {
+  beforeEach(module('TasksManager'));
+
+  var ctrl;
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('guillaumeToDo');
+  }));
+
+  xit('initialises with an empty entry to add new task', function() {
+  });
+
+  xit('can add tasks to the list', function() {
+  });
+
+  xit('knows the exact full list of tasks to display', function() {
+  });
+
+  xit('knows how many tasks there are in the list', function() {
+  });
+
+  xit('knows how many tasks have been completed', function(){
+  });
+
+  xit('knows how many tasks have yet to be completed', function(){
+  });
+
+  xit('can clear and delete tasks from the list', function() {
+  });
+
+  xit('initializes a task as not completed (false)', function(){
+  });
+
+  xit('can switch a task to completed (true)', function(){
+  });
+});
